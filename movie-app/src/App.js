43e6759d47e35e9975e16143dccf6dd2379a949a
alfnbntng <alfanbintang28@gmail.com>
2022-12-12 13:38:59 +0700

    first commit

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,52 +1,99 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Movie from './components/Movie';
+import Movie from "./components/Movie";
 
 function App() {
   // const movies = useState([{ id: 1, title: 'Pengabdi Setan 2'}]);
-  const [ movies, setMovies ] = useState([{ id: 1, title: 'Pengabdi Setan' }]);
-  const [ title, setTitle ] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
 
   const save = () => {
     setMovies((currentState) => {
       //currentState.push({ id: 2, title: title }) // ngga bisa
-      // return currentState.filter() 
-      return [ ...currentState, { id: currentState.length+1, title: title } ]
-    })
-  }
+      // return currentState.filter()
+      return [
+        ...currentState,
+        {
+          id: currentState.length + 1,
+          title: title,
+          author: author,
+          url: url,
+        },
+      ];
+    });
+  };
 
   const edit = (id) => {
-    setMovies(currentState => {
-      return currentState.map(state => {
+    setMovies((currentState) => {
+      return currentState.map((state) => {
         if (state.id === id) {
-          state.title = title;// hanya mengubah value dari key title
+          state.title = title;
+          state.author = author;
+          state.url = url;
+          // hanya mengubah value dari key title
         }
         return state; // tetap return object
-      }) 
-    })
-  }
-
+      });
+    });
+  };
 
   return (
-    <div>
-      <h1>Hello Wolrd</h1>
-      <input 
-        type="text" 
-        placeholder='title' 
-        value={title} 
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <button onClick={save}>Save</button>
-      {
-        movies.map(movie => (
-          <Movie key={movie.id} title={movie.title}/>
-        ))
-      }
-      
+    <div className="tepi">
+      <h1 className="font">Alfan Bookstore</h1>
+      <br />
+      <br />
+      <div className="form-outline mb-4">
+        <input
+          type="text"
+          id="form5Example1"
+          className="form-control"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label className="form-label font" for="form5Example1">
+          <u>Masukan Judul Buku</u>
+        </label>
+      </div>
+
+      <div className="form-outline mb-4">
+        <input
+          type="text"
+          id="form5Example2"
+          className="form-control"
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <label className="form-label font" for="form5Example2">
+          <u>Masukan Author</u>
+        </label>
+      </div>
+      <div className="form-outline mb-4">
+        <input
+          type="url"
+          id="form5Example2"
+          className="form-control"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <label className="form-label font" for="form5Example2">
+          <u>Masukan Link Gambar</u>
+        </label>
+      </div>
+      <button
+        type="submit"
+        onClick={save}
+        className="btn btn-warning btn-block mb-4"
+      >
+        Pilih
+      </button>
+      {movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          author={movie.author}
+          url={movie.url}
+        />
+      ))}
     </div>
   );
 }
-
-
-
 export default App;
--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,9 +1,24 @@
-function Movie({ title }) {
-    return(
-      <div>
-        <h1>Movie: {title}</h1>
+function Movie({ title, author, url }) {
+  return (
+    <div className="col">
+      <div class="card-group">
+        <div class="card">
+          <img src={url} class="card-img-top" alt="..." />
+          <div class="card-body bg-warning">
+            <h5 class="card-title text-light">{title}</h5>
+            <p class="card-text bg-warning text-light">
+              Mariposa adalah novel fiksi ilmiah yang ditulis oleh Greg Bear
+              sebagai sekuel Quantico dan prekuel Queen of Angels, menampilkan
+              karakter dari kedua karya sebelumnya. Wikipedia
+            </p>
+            <p class="card-text text-light">
+              <small class=" text-light">{author}</small>
+            </p>
+          </div>
+        </div>
       </div>
-    )
+    </div>
+  );
 }
 
 export default Movie;
